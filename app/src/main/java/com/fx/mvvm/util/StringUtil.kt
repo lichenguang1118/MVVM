68dd6cc048dbee6bcf400d3c21922d98eb573d0b
lichenguang1118 <465121736@qq.com>
2021-09-30 10:34:10 +0800

    项目提交

--- app/src/main/java/com/fx/mvvm/util/StringUtil.kt
+++ app/src/main/java/com/fx/mvvm/util/StringUtil.kt
@@ -1,23 +1,17 @@
-package com.fx.mvvm.util;
+package com.fx.mvvm.util
 
 /**
  * @Author : chenguang
  * @Time : On 2021/7/21 0021 9:38
  * @Description : StringUtil
  */
-public class StringUtil {
-
-    private static final String TAG = "StringUtil";
-
-    public StringUtil() {
-    }
-
-    public static boolean isIDCard(String number) {
+object StringUtil {
+    fun isIDCard(number: String): Boolean {
         //身份证的正则,18位或15位
-        String regexIDCard = "(^[1-9]\\d{5}(18|19|([23]\\d))\\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\\d{3}[0-9Xx]$)|(^[1-9]\\d{5}\\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\\d{2}[0-9Xx]$)";
-        return number.matches(regexIDCard);
+        val regexIDCard =
+            "(^[1-9]\\d{5}(18|19|([23]\\d))\\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\\d{3}[0-9Xx]$)|(^[1-9]\\d{5}\\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\\d{2}[0-9Xx]$)".toRegex()
+        return number.matches(regexIDCard)
     }
-
 
     /**
      * 移动：134、135、136、137、138、139、150、151、157(TD)、158、159、187、188
@@ -25,9 +19,9 @@ public class StringUtil {
      * 电信：133、153、180、189、（1349卫通）
      * 总结起来就是第一位必定为1，第二位必定为3或5或8，其他位置的可以为0-9
      */
-    public static boolean isMobile(String mobiles) {
-        String telRegex = "(?:(?:(?:13[0-9])|(?:14[57])|(?:15[0-35-9])|(?:17[36-8])|(?:18[0-9]))\\d{8})|(?:170|171[057-9]\\d{7})";//"[1][358]\\d{9}";//"[1]"代表第1位为数字1，"[358]"代表第二位可以为3、5、8中的一个，"\\d{9}"代表后面是可以是0～9的数字，有9位。
-        return mobiles.matches(telRegex);
+    fun isMobile(mobiles: String): Boolean {
+        val telRegex =
+            "(?:(?:(?:13[0-9])|(?:14[57])|(?:15[0-35-9])|(?:17[36-8])|(?:18[0-9]))\\d{8})|(?:170|171[057-9]\\d{7})".toRegex() //"[1][358]\\d{9}";//"[1]"代表第1位为数字1，"[358]"代表第二位可以为3、5、8中的一个，"\\d{9}"代表后面是可以是0～9的数字，有9位。
+        return mobiles.matches(telRegex)
     }
-
 }
